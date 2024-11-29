b19a39d0fc301a562d841b2de0072bd3b80d15b1
Mikel-Ma <116980451+Mikel-Ma@users.noreply.github.com>
2024-11-29 12:59:54 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,23 @@ ext_modules = [
 
 setup(
     name='spex-tequila',
-    version='0.1',
+    version='0.0.1',
     author='Michael Lang',
-    description='Expectation value computation module on sparse pauli states for tequila, implemented in C++ using Pybind11',
+    author_email='lang-michi@t-online.de', 
+    url='https://git.rz.uni-augsburg.de/qalg-a/spex', 
+    description='Expectation value computation module on sparse Pauli states for Tequila, implemented in C++ using Pybind11',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
     zip_safe=False,
+    python_requires='>=3.6',
+    install_requires=[
+        'pybind11>=2.5.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C++',
+        'Operating System :: OS Independent',
+    ],
 )
