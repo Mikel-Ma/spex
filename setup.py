e38fbf4684d3c06bce60fc77de88f2bcb5cd3b77
Mikel-Ma <116980451+Mikel-Ma@users.noreply.github.com>
2024-11-29 15:04:09 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,41 @@
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import pybind11
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path"""
+    def __str__(self):
+        return pybind11.get_include()
+    
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'unix': ['-O3', '-std=c++17'],
+    }
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
 
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         'spex_tequila',
         ['spex.cpp'],
+        include_dirs=[
+            get_pybind_include(),
+        ],
+        language='c++',
     ),
 ]
 
 setup(
     name='spex-tequila',
-    version='0.0.1',
+    version='0.0.4',
     author='Michael Lang',
     author_email='lang-michi@t-online.de', 
     url='https://git.rz.uni-augsburg.de/qalg-a/spex', 
@@ -22,7 +47,8 @@ setup(
     zip_safe=False,
     python_requires='>=3.6',
     install_requires=[
-        'pybind11>=2.5.0'
+        'pybind11>=2.5.0',
+        'tequila'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -30,4 +56,5 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
     ],
+    packages=find_packages(),
 )
